a674e8886d0304ea59643b41e2fe5f8044cb1d85
Shin-hima <88337028+Shin-hima@users.noreply.github.com>
2021-08-04 21:27:05 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Evil Xobot 
-:: Enables virtual env mode and then starts Evil
-env\scripts\activate.bat && py -m EvilBott
+TITLE shinsanBot 
+:: Enables virtual env mode and then starts shinsan
+env\scripts\activate.bat && py -m shinsan
